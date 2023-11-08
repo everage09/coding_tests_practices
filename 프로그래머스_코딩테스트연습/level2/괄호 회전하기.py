723a1294bd158ece623f459f6698c160ab9d1403
everage09 <jeongmh09@naver.com>
2023-11-09 01:45:00 +0900

    brackets rotation

---
+++ 프로그래머스_코딩테스트연습/level2/괄호 회전하기.py
@@ -0,0 +1,43 @@
+# 괄호 회전하기
+
+"""
+다음 규칙을 지키는 문자열을 올바른 괄호 문자열이라고 정의합니다.
+
+(), [], {} 는 모두 올바른 괄호 문자열입니다.
+
+만약 A가 올바른 괄호 문자열이라면, (A), [A], {A} 도 올바른 괄호 문자열입니다.
+예를 들어, [] 가 올바른 괄호 문자열이므로, ([]) 도 올바른 괄호 문자열입니다.
+
+만약 A, B가 올바른 괄호 문자열이라면, AB 도 올바른 괄호 문자열입니다.
+예를 들어, {} 와 ([]) 가 올바른 괄호 문자열이므로, {}([]) 도 올바른 괄호 문자열입니다.
+
+대괄호, 중괄호, 그리고 소괄호로 이루어진 문자열 s가 매개변수로 주어집니다.
+
+이 s를 왼쪽으로 x (0 ≤ x < (s의 길이)) 칸만큼 회전시켰을 때
+s가 올바른 괄호 문자열이 되게 하는 x의 개수를 return 하도록 solution 함수를 완성해주세요.
+"""
+
+# s의 길이는 1 이상 1,000 이하입니다.
+
+# tried not to import module.
+# would be better using deque
+def solution(s):
+    answer = 0
+    stack = []
+    for shift in range(len(s)):
+        stream = s[shift:] + s[:shift]
+        for letter in stream:
+            stack.append(letter)
+            while len(stack) > 1 :
+                top2 = [stack[-2],stack[-1]]
+                if  top2 in [["(",")"],
+                            ["{","}"],
+                            ["[","]"]]:
+                    stack.pop()
+                    stack.pop()
+                else:
+                    break
+        if len(stack) == 0:
+            answer += 1
+        stack.clear()
+    return answer
